99f6d2de6e29c85e6480ccfefb7a9f75892f1325
ShuntaroItakura <i2432008@gl.cc.uec.ac.jp>
2024-05-13 18:55:35 +0900

    create 直角左右

--- turtlebot_control.py
+++ turtlebot_control.py
@@ -1,38 +1,98 @@
 #!/usr/bin/env python
 import rospy
+from std_msgs.msg import String
+import yaml
+import time
 from geometry_msgs.msg import Twist
 
-def move_turtle(command):
-    # Create a new Twist message
-    twist_msg = Twist()
+grammar = """
+[GRAMMAR]
+greeding : $slot_greeding
+bring_known_obj : $slot_drink * $slot_person * <持って行って|届けて|取って>
+bring_unknown_obj : $slot_any を $slot_person * <持って行って|届けて|取って>
 
-    # Set linear and angular velocities based on the command
-    if command == "前":
-        twist_msg.linear.x = 0.2  # Forward velocity
-    elif command == "右":
-        twist_msg.angular.z = -0.2  # Clockwise angular velocity
-    elif command == "左":
-        twist_msg.angular.z = 0.2  # Counter-clockwise angular velocity
-    elif command == "後ろ":
-        twist_msg.linear.x = -0.2  # Backward velocity
-    elif command == "止まれ":
-        twist_msg.linear.x = 0.0  # Stop linear motion
-        twist_msg.angular.z = 0.0  # Stop angular motion
-    else:
-        rospy.logwarn("Unknown command: %s", command)
-        return
 
-    # Publish the Twist message to control the turtle
-    twist_pub.publish(twist_msg)
+
+
+[SLOT]
+$slot_greeding
+forward : 前
+right : 右
+left : 左
+backward:後ろ
+stop:止まれ
+ninetyleft:レフト
+ninetyright:ライト
+
+$slot_drink
+drink1 : ジュース
+drink2 : コーラ|コーク
+drink3 : お茶|緑茶
+
+$slot_person
+person1 : 中村|中村さん
+person2 : 田中
+person3 : 太郎
+
+"""
 
 def main():
-    rospy.init_node('turtle_control')
-    rospy.Subscriber('voice_commands', String, move_turtle)
-    rospy.spin()
+    rospy.init_node('gspeech_example2')
+
+
+    pub_grammar = rospy.Publisher( "grammar_lu/grammar", String, queue_size=10, latch=True )
+    pub_synthesis = rospy.Publisher( "google_speech/utterance", String, queue_size=10 )	
+
+    time.sleep(2)
+    pub_grammar.publish( grammar )
+
+
+    while not rospy.is_shutdown():
+        msg = rospy.wait_for_message( 'grammar_lu/results',  String )
+        results = yaml.safe_load(msg.data)
+        twist_msg = Twist()
+
+        print( results["text"] )
+        print( results["gram_id"] )
+        print( results["slot_id"] )
+        print( results["slot_str"] )
+        print("--------")
+
+        if results["gram_id"]=="greeding":
+            if results["slot_id"][0]=="forward":
+                twist_msg.linear.x += 0.1  # Forward velocity
+            elif results["slot_id"][0]=="right":
+                twist_msg.angular.z += -0.2  # Clockwise angular velocity
+            elif results["slot_id"][0]=="left":
+                twist_msg.angular.z += 0.2  # Counter-clockwise angular velocity
+            elif results["slot_id"][0]=="backward":
+                twist_msg.linear.x += -0.1  # Backward velocity
+            elif results["slot_id"][0]=="stop":
+                twist_msg.linear.x = 0.0  # Stop linear motion
+                twist_msg.angular.z = 0.0  # Stop angular motione
+            elif results["slot_id"][0]=="ninetyright":
+                twist_msg.angular.z += -0.2
+                print(twist_msg)
+                twist_pub.publish(twist_msg)
+                time.sleep(7.85)
+                twist_msg.linear.x = 0.0  # Stop linear motion
+                twist_msg.angular.z = 0.0  # Stop angular motione
+            elif results["slot_id"][0]=="ninetyleft":
+                twist_msg.angular.z += 0.2
+                print(twist_msg)
+                twist_pub.publish(twist_msg)
+                time.sleep(7.85)
+                twist_msg.linear.x = 0.0  # Stop linear motion
+                twist_msg.angular.z = 0.0  # Stop angular motione
+            else:
+                rospy.logwarn("Unknown command: %s", command)
+            
+        print(twist_msg)
+        twist_pub.publish(twist_msg)
+
+
+
 
 if __name__ == '__main__':
-    try:
-        twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        main()
-    except rospy.ROSInterruptException:
-        pass
+    twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    main()
